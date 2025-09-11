a59b6f504aa5dd12a8b5931d1e53bf6a48bf1a77
Lucas Ganem <lucasvganem@gmail.com>
2025-09-10 21:25:09 -0300

    Modificaçoes iniciais

--- servidor_udp_trab_1.py
+++ servidor_udp_trab_1.py
@@ -1,5 +1,11 @@
 from socket import *
 
+def get_file():
+        serverSocket.sendto(b"GET acionado", clientAddress)
+
+def retrieve_data():
+        serverSocket.sendto(b"RETR acionado", clientAddress)
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
@@ -7,7 +13,15 @@ print("Servidor pronto\n")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print(clientAddress)
-    #modifiedMessage = message.decode().upper()
-    #serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+        
+    if not message:
+        continue
+    text = message.decode(errors="ignore").strip()
+    if text.startswith("GET "):
+        get_file()
+    elif text.startswith("RETR "):
+        retrieve_data()
+    else:        
+        serverSocket.sendto(b"ERR 400 Unknown command", clientAddress)
 
     
